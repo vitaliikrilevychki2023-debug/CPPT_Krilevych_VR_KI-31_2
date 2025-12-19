0003bd9bfd6bdeb6519fa49ec6574e9b499f5e4b
Vitalii <vitalii.krilevych.ki.2023@lpnu.ua>
2025-12-19 22:42:48 +0200

    Код працює)

---
+++ LAB_02/src/KI31/Krilevych/Speaker.java
@@ -0,0 +1,30 @@
+package KI31.Krilevych;
+
+/**
+ * Клас `Speaker` представляє динамік телевізора з можливістю налаштування гучності.
+ */
+public class Speaker {
+    private int maxVolume;
+    private int currentVolume;
+
+    /**
+     * Конструктор для створення динаміка з вказаною максимальною гучністю.
+     *
+     * @param maxVolume Максимальна гучність динаміка.
+     */
+    public Speaker(int maxVolume) {
+        this.maxVolume = maxVolume;
+        this.currentVolume = maxVolume / 2; // За замовчуванням встановлюється половина від максимальної гучності
+    }
+
+    /**
+     * Встановлює нову гучність для динаміка. Якщо вказана гучність перевищує
+     * максимальну, вона автоматично обмежується максимальним значенням.
+     *
+     * @param volume Нова гучність.
+     */
+    public void setVolume(int volume) {
+        this.currentVolume = Math.min(volume, maxVolume);
+    }
+}
+
